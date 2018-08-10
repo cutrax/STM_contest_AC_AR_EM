49fee8e842e6cd0401bafe387c5ef0a01221ed07
Emanuele <Emanuele@Emanuele-PC.station>
2018-08-10 18:16:48 +0200

    ADCnew

--- USART_test1/Libraries/myLib/inc/myLib.h
+++ USART_test1/Libraries/myLib/inc/myLib.h
@@ -11,10 +11,13 @@
 #include "stm32f4xx_usart.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
+#include "stm32f4xx_adc.h"
 
 
 void myUSART2_Init(void);
 void myLED_Init(void);
 void mySWITCH_Init(void);
+void myADC_Init(void);
+void ADC_IRQnHandler(void);
 
 #endif /* MYLIB_INC_MYLIB_H_ */
--- USART_test1/Libraries/myLib/src/myLib.c
+++ USART_test1/Libraries/myLib/src/myLib.c
@@ -10,14 +10,67 @@
 #include "myLib.h"
 
 /*
+ * myADC_Init
+ * Inizializza e abilita l'ADC.
+ * Il micro che usiamo noi (f401) possiede solamente un ADC, che dalle librerie è definito
+ * come ADC1.
+ */
+
+
+void myADC_Init(void)
+{
+ADC_InitTypeDef ADC_InitStructure;
+ADC_CommonInitTypeDef ADC_CommonInitStructure;
+NVIC_InitTypeDef NVIC_InitStructure;
+
+RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE); //Abilito il clock per l'ADC
+
+ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;
+ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+ADC_CommonInit(&ADC_CommonInitStructure);
+
+ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+ADC_InitStructure.ADC_NbrOfConversion = 10;
+ADC_Init(ADC1, &ADC_InitStructure);
+
+ADC_ContinuousModeCmd(ADC1, ENABLE); //Abilito il modo di conversione continuo
+ADC_RegularChannelConfig(ADC1, ADC_Channel_TempSensor  , 1, ADC_SampleTime_15Cycles); //Attivo il canale 16
+ADC_TempSensorVrefintCmd(ENABLE); //Abilito il sensore di temperatura
+ADC_EOCOnEachRegularChannelCmd(ADC1, ENABLE); //Abilito l'end of conversion
+
+ADC_ITConfig(ADC1, ADC_IT_EOC, ENABLE); //Abilito l'interrupt su EOC
+//ADC_TempSensorVrefintCmd(ENABLE); //Abilito il sensore di temperatura
+  //ADC_Cmd(ADC1, ENABLE); //Abilito l'ADC
+
+  //Abilito le interruzioni provenienti dall'ADC
+  NVIC_InitStructure.NVIC_IRQChannel = ADC_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+
+ADC_Cmd(ADC1, ENABLE); //Abilito l'ADC
+
+}
+
+/*
  * myUSART2_Init
  * Inizializza la USART2 con i relativi pin PA2(TX) e PA3(RX)
  * Inizializza anche il clock a AHB1_GPIOA e APB1_USART2
  */
+
 void myUSART2_Init(void)
 {
 	USART_InitTypeDef usart2Init;
 	GPIO_InitTypeDef pa2Init,pa3Init;
+
 
 	//INIZIO configurazione della porta A
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
