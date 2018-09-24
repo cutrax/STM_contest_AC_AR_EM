f3c1cee02c19461e47cea5a65d9f7a9d21885f1e
cicci <cicci@LAPTOP-74JRALD6.homenet.telecomitalia.it>
2018-09-25 01:08:10 +0200

    Mi ero dimenticato di fare il merge del pezzo delle nuove FFT nel main

--- Blue/Src/main.c
+++ Blue/Src/main.c
@@ -232,6 +232,65 @@ int main(void)
 				fft_Z[i].im /= n_C;
 			}
 
+			//Calcolo campioni da inviare all'app tramite BLE
+			float fftx1_6, fftx7_12, fftx13_18, fftx19_24, fftx25_30,
+			      ffty1_6, ffty7_12, ffty13_18, ffty19_24, ffty25_30,
+				  fftz1_6, fftz7_12, fftz13_18, fftz19_24, fftz25_30;
+
+			fft_t* i_frequency;
+
+
+
+			for(int j=0; j<=30; j++)
+			{
+				if(j<=6) { fftx1_6 += fft_X[j].re*fft_X[j].re + fft_X[j].im*fft_Y[j].im;
+				           ffty1_6 += fft_Y[j].re*fft_Y[j].re + fft_Y[j].im*fft_Y[j].im;
+				           fftz1_6 += fft_Z[j].re*fft_Z[j].re + fft_Z[j].im*fft_Z[j].im;}
+
+				if(j<=12) { fftx7_12 += fft_X[j].re*fft_X[j].re + fft_X[j].im*fft_Y[j].im;
+				            ffty7_12 += fft_Y[j].re*fft_Y[j].re + fft_Y[j].im*fft_Y[j].im;
+				            fftz7_12 += fft_Z[j].re*fft_Z[j].re + fft_Z[j].im*fft_Z[j].im;}
+
+				if(j<=18) { fftx13_18 += fft_X[j].re*fft_X[j].re + fft_X[j].im*fft_Y[j].im;
+				            ffty13_18 += fft_Y[j].re*fft_Y[j].re + fft_Y[j].im*fft_Y[j].im;
+				            fftz13_18 += fft_Z[j].re*fft_Z[j].re + fft_Z[j].im*fft_Z[j].im;}
+
+				if(j<=24) { fftx19_24 += fft_X[j].re*fft_X[j].re + fft_X[j].im*fft_Y[j].im;
+				            ffty19_24 += fft_Y[j].re*fft_Y[j].re + fft_Y[j].im*fft_Y[j].im;
+				            fftz19_24 += fft_Z[j].re*fft_Z[j].re + fft_Z[j].im*fft_Z[j].im;}
+
+				if(j<=30) { fftx25_30 += fft_X[j].re*fft_X[j].re + fft_X[j].im*fft_Y[j].im;
+				            ffty25_30 += fft_Y[j].re*fft_Y[j].re + fft_Y[j].im*fft_Y[j].im;
+				            fftz25_30 += fft_Z[j].re*fft_Z[j].re + fft_Z[j].im*fft_Z[j].im;}
+			}
+
+			fftx1_6 = sqrtf(fftx1_6);
+			fftx7_12 = sqrtf(fftx7_12);
+			fftx13_18 = sqrtf(fftx13_18);
+			fftx19_24 = sqrtf(fftx19_24);
+			fftx25_30 = sqrtf(fftx25_30);
+
+			ffty1_6 = sqrtf(ffty1_6);
+			ffty7_12 = sqrtf(ffty7_12);
+			ffty13_18 = sqrtf(ffty13_18);
+			ffty19_24 = sqrtf(ffty19_24);
+			ffty25_30 = sqrtf(ffty25_30);
+
+			fftz1_6 = sqrtf(fftz1_6);
+			fftz7_12 = sqrtf(fftz7_12);
+			fftz13_18 = sqrtf(fftz13_18);
+			fftz19_24 = sqrtf(fftz19_24);
+			fftz25_30 = sqrtf(fftz25_30);
+
+			//Valori da mandare in pasto al BLE
+			i_frequency.FREQUENCY1_6 = sqrtf((fftx1_6*fftx1_6) + (ffty1_6*ffty1_6) + (fftz1_6*fftz1_6));
+			i_frequency.FREQUENCY7_12 = sqrtf((fftx7_12*fftx7_12) + (ffty7_12*ffty7_12) + (fftz7_12*fftz7_12));
+			i_frequency.FREQUENCY13_18 = sqrtf((fftx13_18*fftx13_18) + (ffty13_18*ffty13_18) + (fftz13_18*fftz13_18));
+			i_frequency.FREQUENCY19_24 = sqrtf((fftx19_24*fftx19_24) + (ffty19_24*ffty19_24) + (fftz19_24*fftz19_24));
+			i_frequency->FREQUENCY25_30 = sqrtf((fftx25_30*fftx25_30) + (ffty25_30)*(ffty25_30) + (fftz25_30*fftz25_30));
+
+
+
  //			printf("Stampo i valori della DC:\r\n");
  //			printf("X:%f,Y:%f,Z:%f\r\n",fft_X[0].re,fft_Y[0].re,fft_Z[0].re);
 		 //   printf("FFT terminata\r\n");
